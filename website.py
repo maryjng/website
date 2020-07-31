37a0b1625b959c7ad51bd2e386b8049836229814
maryjng <68235230+maryjng@users.noreply.github.com>
2020-07-31 13:02:54 -0400

    Update website.py

--- website.py
+++ website.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
+
 app = FLASK(__name__)
 
 #always do set FLASK_APP=website.py in command first
 
+@app.route('/')
+    def index():
